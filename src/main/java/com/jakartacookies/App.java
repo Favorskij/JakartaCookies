f1e152bb641409dedc372602cd727491f4aefee3
favorskij <favorskij@mail.ru>
2020-10-25 19:41:40 +1100

    Commit

---
+++ src/main/java/com/jakartacookies/App.java
@@ -0,0 +1,8 @@
+package com.jakartacookies;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class App extends Application {
+}
--- src/main/java/com/jakartacookies/NewCookies.java
+++ src/main/java/com/jakartacookies/NewCookies.java
@@ -1,6 +1,7 @@
 package com.jakartacookies;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,20 +12,35 @@ import javax.ws.rs.core.MediaType;
 @Path("/")
 public class NewCookies {
 
+    private String cookie;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN + ";charset=utf8")
-    public String newCookies(@Context HttpServletResponse response) {
+    public String newCookies(@Context HttpServletRequest request, @Context HttpServletResponse response) {
 
         // Создаём куки
-        Cookie cookie = new Cookie("_cookies", "ThisCoockie");
-        cookie.setMaxAge(-1); // В секундах. -1 на время открытого браузера. 24 * 60 * 60 это сутки. 0 удаляет.
-        cookie.setPath("/");
-        cookie.setDomain("localhost");
-        cookie.setSecure(false);
+        Cookie cookie1 = new Cookie("_cookies", "ThisCoockie");
+        cookie1.setMaxAge(-1); // В секундах. -1 на время открытого браузера. 24 * 60 * 60 это сутки. 0 удаляет.
+        cookie1.setPath("/");
+        cookie1.setDomain("localhost");
+        cookie1.setSecure(false);
 
         // Вставляем подготовленные Cookies в ответ response
-        response.addCookie(cookie);
+        response.addCookie(cookie1);
 
-        return "Привет мир!!!";
+
+        // Получаем куки
+        Cookie[] cookies = request.getCookies();
+        String cookieName = "_cookies";
+        if(cookies !=null) {
+            for(Cookie c: cookies) {
+                if(cookieName.equals(c.getName())) {
+                    this.cookie = c.getName() + " " + c.getValue();
+                    break;
+                }
+            }
+        }
+
+        return cookie;
     }
 }
